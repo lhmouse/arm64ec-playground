0f6bf8e95253d8f6b8eb3ad867a49815f44355fb
LIU Hao <lh_mouse@126.com>
2025-12-19 20:37:17 +0800

    script

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,14 @@
 
 clang --target=x86_64-w64-mingw32 -masm=intel m/*.c -nostdlib -shared -o libm.dll
 clang++ --target=x86_64-w64-mingw32 -static test.cpp libm.dll -o test.exe
-./test.exe
+./test.exe  # good
 
-clang --target=arm64ec-w64-mingw32 m/*.c -nostdlib -shared -o libm.dll -Wl,--gc-sections
-./test.exe
+clang --target=arm64ec-pc-windows-msvc m/gthr_aux.c -S -o gthr_aux.s
+clang --target=arm64ec-pc-windows-msvc gthr_aux.s -c
+clang --target=arm64ec-w64-mingw32 gthr_aux.o m/once.c -nostdlib -shared -o libm.dll -Wl,--gc-sections
+./test.exe  # good
+
+clang --target=arm64ec-pc-windows-msvc m/gthr_aux.c -S -o gthr_aux.s
+clang --target=arm64ec-w64-mingw32 gthr_aux.s -c
+clang --target=arm64ec-w64-mingw32 gthr_aux.o m/once.c -nostdlib -shared -o libm.dll -Wl,--gc-sections
+./test.exe  # bad
