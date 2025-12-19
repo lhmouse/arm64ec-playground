05bd1a99b51add1d4337710a0f325a443ff6d378
LIU Hao <lh_mouse@126.com>
2025-12-19 19:04:21 +0800

    remove system libs

--- run_test.sh
+++ run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -x
 
-clang --target=x86_64-w64-mingw32 -masm=intel mcfgthread/*.c -nostdlib -lkernel32 -lntdll -shared -o libmcfgthread.dll
+clang --target=x86_64-w64-mingw32 -masm=intel mcfgthread/*.c -nostdlib -shared -o libmcfgthread.dll
 clang++ --target=x86_64-w64-mingw32 -static test.cpp libmcfgthread.dll -o test.exe
 ./test.exe
 
-clang --target=arm64ec-w64-mingw32 mcfgthread/*.c -nostdlib -lkernel32 -lntdll -shared -o libmcfgthread.dll -Wl,--gc-sections
+clang --target=arm64ec-w64-mingw32 mcfgthread/*.c -nostdlib -shared -o libmcfgthread.dll -Wl,--gc-sections
 ./test.exe
