8a4515e0f6365d736e6618596843490d53ac58a2
LIU Hao <lh_mouse@126.com>
2025-12-19 11:22:53 +0800

    simplify

---
+++ test.cpp
@@ -0,0 +1,24 @@
+#include "../mcfgthread/gthr_aux.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static
+void
+once_do_it(void* add)
+  {
+    ::fprintf(stderr, "throwing\n");
+    throw 42;
+  }
+
+int
+main(void)
+  {
+    ::fprintf(stderr, "main\n");
+    try {
+      ::_MCF_once once = {};
+      ::__MCF_gthr_call_once_seh(&once, once_do_it, (void*) 1);
+      ::abort();
+    }
+    catch(...) { }
+    ::fprintf(stderr, "done\n");
+  }
